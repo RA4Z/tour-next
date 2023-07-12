39a7b356f83ee6e03c3f166453c8e01c0011c3bc
RA4Z <rraz639@gmail.com>
2023-07-12 12:33:48 -0300

    Static Site Generation

--- aula02/a2.1/pages/posts/[id].js
+++ aula02/a2.1/pages/posts/[id].js
@@ -1,16 +1,42 @@
 import NextLink from 'next/link';
 import { Box, Text } from '@skynexui/components';
 import { useRouter } from 'next/router';
+import dados from '../../dados.json';
 
-export default function PostByIdScreen() {
-  const router = useRouter();
+export async function getStaticPaths() {
+  const paths = dados.posts.map((postAtual) => {
+    return { params: { id: `${postAtual.id}` } }
+  })
+  return {
+    paths: paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id
+  const post = dados.posts.find((currentPost) => {
+    if (currentPost.id === id) {
+      return true
+    }
+    return false
+
+  })
+  return {
+    props: {
+      id: post.id,
+      title: post.title,
+      date: post.date,
+      content: post.content
+    },
+  }
+}
+
+export default function PostByIdScreen(props) {
   const post = {
-    title: `Post: [${router.query.id}]`,
-    date: `11/11/1111`,
-    content: `
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lorem urna, laoreet sed magna ac, scelerisque rhoncus nulla. Morbi rhoncus venenatis elementum. Phasellus nec purus ut nisi tempus sagittis sed eget lacus. Nullam in nibh porttitor, dictum metus placerat, molestie augue. Phasellus a mi fermentum erat tempor sollicitudin. Donec eget elementum leo. Morbi sit amet enim varius, ultrices leo commodo, faucibus purus.
-      Vestibulum ut sem est. Phasellus sodales rhoncus convallis. Etiam sit amet libero non tellus vestibulum bibendum. Integer ac eros vel justo imperdiet dictum. Etiam dapibus dolor augue, a suscipit metus sollicitudin in. Integer suscipit ac justo eget molestie. Vivamus id auctor lectus. Ut malesuada est quis urna mollis semper. Nulla cursus, metus eget elementum venenatis, tortor massa tincidunt justo, ac faucibus tortor felis id lacus. Sed quis dolor sem. Sed cursus justo eu accumsan rhoncus. Nam eget lectus libero. Nulla nulla leo, iaculis sagittis massa non, malesuada porta velit. Fusce vel accumsan nisi.
-    `,
+    title: props.title,
+    date: props.date,
+    content: props.content,
   }
 
   return (
@@ -44,7 +70,7 @@ export default function PostByIdScreen() {
           {post.content}
         </Text>
 
-        {post.video && <iframe style={{ marginTop: '32px', minHeight: '400px' }} src={post.video} /> }
+        {post.video && <iframe style={{ marginTop: '32px', minHeight: '400px' }} src={post.video} />}
       </Box>
 
 
